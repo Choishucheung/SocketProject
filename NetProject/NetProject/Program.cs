39be1b0fc43843773654636757042a61ea85a1a8
蔡书翔 <choishucheung@outlook.com>
2018-07-03 01:12:08 +0800

    上传服务器端

---
+++ NetProject/NetProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace NetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 88);
+            serverSocket.Bind(iPEndPoint);//绑定ip和端口号 申请
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
